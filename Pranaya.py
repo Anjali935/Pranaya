36e5cdaf0be63d92b225be7817779500bcebdc27
Narendra404 <npaul7345@gmail.com>
2023-03-22 06:25:19 +0000

    getResponses function argument modifiy

--- Pranaya.py
+++ Pranaya.py
@@ -88,7 +88,7 @@ def get_text():
 user_input = get_text()
 
 if user_input:
-    output = getResponse(user_input)
+    output = getResponse(user_input, intents)
     # store the output
     st.session_state.past.append(user_input)
     st.session_state.generated.append(output)
