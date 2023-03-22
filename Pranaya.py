dadd2f99ab65176244ddbf0d018d924269f89f84
Narendra404 <npaul7345@gmail.com>
2023-03-22 05:59:53 +0000

    Modify Pranaya.py to include streamlit changes

--- Pranaya.py
+++ Pranaya.py
@@ -4,6 +4,10 @@ lemmatizer = WordNetLemmatizer()
 import pickle
 import numpy as np
 
+import streamlit as st
+# pip install streamlit-chat
+from streamlit_chat import message
+
 from keras.models import load_model
 model = load_model('chatbot_model.h5')
 import json
@@ -60,3 +64,38 @@ def chatbot_response(text):
     ints = predict_class(text, model)
     res = getResponse(ints, intents)
     return res
+
+def generate_response(prompt):
+    # completions = 
+    message = "Hello my name is Narendra"
+    return message
+
+#Creating the chatbot interface
+st.title("chatBot : Streamlit + openAI")
+
+# Storing the chat
+if 'generated' not in st.session_state:
+    st.session_state['generated'] = []
+
+if 'past' not in st.session_state:
+    st.session_state['past'] = []
+
+# We will get the user's input by calling the get_text function
+def get_text():
+    input_text = st.text_input("You: ","Hello, how are you?", key="input")
+    return input_text
+
+user_input = get_text()
+
+if user_input:
+    output = getResponse(user_input)
+    # store the output
+    st.session_state.past.append(user_input)
+    st.session_state.generated.append(output)
+
+if st.session_state['generated']:
+
+    for i in range(len(st.session_state['generated'])-1, -1, -1):
+        message(st.session_state["generated"][i], key=str(i))
+        message(st.session_state['past'][i], is_user=True, key=str(i) + '_user')
+
